61f2c3859527101994a28c928f4b11916dd3157f
Will Trojniak <william.trojniak@gmail.com>
2024-02-05 15:30:09 -0500

    Remove duplicate options settings

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,15 +1,3 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
-local set = vim.opt
-
-set.expandtab = true
-set.tabstop = 2
-set.softtabstop = 2
-set.shiftwidth = 2
-set.number = true
-set.relativenumber = true
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
