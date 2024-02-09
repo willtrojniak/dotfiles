3063dd940ceaacd1866b683319b03b5c9b904388
Will <william.trojniak@gmail.com>
2024-02-09 09:25:25 -0500

    Update vim options
    Key changes:
    1. Add line highlight
    2. Always keep lines above/below cursor
    3. Disable line wrapping

--- config/nvim/lua/vim-options/init.lua
+++ config/nvim/lua/vim-options/init.lua
@@ -1,13 +1,40 @@
+-- Set leader key to space
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 local set = vim.opt
 
-set.expandtab = true
-set.tabstop = 2
-set.softtabstop = 2
-set.shiftwidth = 2
+-- Enable relative line numbers
 set.number = true
 set.relativenumber = true
 
+-- Set tabs to two spaces
+set.expandtab = true
+set.tabstop = 2
+set.softtabstop = 2
 
+-- Enable auto indenting and set it to spaces
+set.smartindent = true
+set.shiftwidth = 2
+
+-- Enable ignorecase and smartcase for better searching
+set.ignorecase = true
+set.smartcase = true
+
+-- Enable persistent undo history
+set.undofile = true
+
+-- Enable 24-bit color
+set.termguicolors = true
+
+-- Enable sign column always
+set.signcolumn = "yes"
+
+-- Disable line wrapping
+set.wrap = false
+
+-- Enable cursor line highlight
+set.cursorline = true
+
+-- Always keep at least 8 lines above/below cursor
+set.scrolloff = 12
