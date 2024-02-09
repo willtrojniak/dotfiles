ac8e2af22f262bffdff753edcfc7cf404cb27e51
Will <william.trojniak@gmail.com>
2024-02-09 09:33:14 -0500

    Add documentation

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -5,13 +5,27 @@ return {
   config = function()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
+
+      -- Install parsers asynchronously
+      sync_install = false,
+
+      -- Ensure that the following parsers are always installed
       ensure_installed = {
         "lua",
         "javascript",
         "typescript",
         "tsx"
       },
+
+
+
+
+      ignore_install = {},
+
+      -- Automatically install missing parsers when entering buffer
       auto_install = true,
+
+      modules = {},
       highlight = { enable = true },
       indent = { enable = true },
     })
