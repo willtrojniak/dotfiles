a103d8258db269c51308cd8b0948e3d62e17f968
Will <william.trojniak@gmail.com>
2024-04-26 12:55:44 -0400

    QOL Features

--- config/nvim/lua/vim-options/init.lua
+++ config/nvim/lua/vim-options/init.lua
@@ -40,3 +40,33 @@ set.cursorline = true
 
 -- Always keep at least 8 lines above/below cursor
 set.scrolloff = 12
+
+-- Case-insensitive searching unless \C or capital letter in search term
+set.ignorecase = true
+set.smartcase = true
+
+-- Highlight on search, clear on pressing <Esc> in normal mode
+set.hlsearch = true
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Decrease update time
+set.updatetime = 250
+
+-- Decrease mapped sequence wait time
+set.timeoutlen = 300
+
+-- Remove arrow key functionality
+vim.keymap.set({'n', 'v'}, '<Left>', '<Nop>')
+vim.keymap.set({'n', 'v'}, '<Right>', '<Nop>')
+vim.keymap.set({'n', 'v'}, '<Up>', '<Nop>')
+vim.keymap.set({'n', 'v'}, '<Down>', '<Nop>')
+
+
+-- Highlight when yanking text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking text',
+  group = vim.api.nvim_create_augroup('highlight-yank', {clear = true}),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
