68a8dceccd35f41737836aa02b5429599f539bad
Will Trojniak <william.trojniak@gmail.com>
2024-05-21 16:30:32 -0400

    add nvim notify

--- config/nvim/lua/plugins/noice.lua
+++ config/nvim/lua/plugins/noice.lua
@@ -2,12 +2,21 @@ return {
   "folke/noice.nvim",
   event = "VeryLazy",
   dependencies = {
-    "MunifTanjim/nui.nvim"
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+    "hrsh7th/nvim-cmp"
   },
   opts = {
-   cmdline = {
-    enabled = true, -- enables the Noice cmdline UI
-    view = "cmdline_popup",
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+      },
+    },
+    cmdline = {
+      enabled = true, -- enables the Noice cmdline UI
+      view = "cmdline_popup",
     }
   },
 }
