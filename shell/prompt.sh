f743be37174a3044882f8992f22933e295115e00
Will <william.trojniak@gmail.com>
2024-05-29 18:16:42 -0400

    set starship conf file

--- shell/prompt.sh
+++ shell/prompt.sh
@@ -1,5 +1,8 @@
 if command -v starship >& /dev/null; then
   # TODO Change between bash and zsh
+  if [ -f "$HOME/.config/starship/starship.toml" ]; then
+    STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
+  fi
   eval "$(starship init bash)"
 else
   export PS1="\w> \e[35m\]\$ \e[0m\]"
