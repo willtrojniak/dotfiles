ba26978af9a6e4c2ef8382fc54c320ff475c5684
Will Trojniak <william.trojniak@gmail.com>
2024-11-28 15:29:03 -0500

    Better charging icons for battery

--- config/sketchybar/plugins/battery.sh
+++ config/sketchybar/plugins/battery.sh
@@ -20,7 +20,29 @@ case "${PERCENTAGE}" in
 esac
 
 if [[ "$CHARGING" != "" ]]; then
-  ICON=""
+  case "${PERCENTAGE}" in
+    100) ICON="󰂅"
+    ;;
+    9[0-9]) ICON="󰂋"
+    ;;
+    8[0-9]) ICON="󰂊"
+    ;;
+    7[0-9]) ICON="󰢞"
+    ;;
+    6[0-9]) ICON="󰂉"
+    ;;
+    5[0-9]) ICON="󰢝"
+    ;;
+    4[0-9]) ICON="󰂈"
+    ;;
+    3[0-9]) ICON="󰂇"
+    ;;
+    2[0-9]) ICON="󰂆"
+    ;;
+    1[0-9]) ICON="󰢜"
+    ;;
+    *) ICON="󰢜"
+  esac
 fi
 
 # The item invoking this script (name $NAME) will get its icon and label
