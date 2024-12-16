6accfb922ed9696d77fae50167a6f6b517e867d8
Will <william.trojniak@gmail.com>
2024-12-16 17:11:49 -0500

    Live rendering of markdown within neovim

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -21,11 +21,12 @@ return {
       mapping = cmp.mapping.preset.insert({
         ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<tab>"] = cmp.mapping.confirm({select = true}),
+        ["<tab>"] = cmp.mapping.confirm({ select = true }),
       }),
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "luasnip" },
+        { name = "render-markdown" }
       }, {
         { name = "buffer" },
       }),
--- config/nvim/lua/plugins/markdown-preview.lua
+++ config/nvim/lua/plugins/markdown-preview.lua
@@ -1,7 +1,21 @@
 return {
+  {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
-}
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
 
+    },
+    ft = { 'markdown' }
+  }
+}
