d2679218b93f5d9f798f30ea19f0e3446a1d112f
Will Trojniak <william.trojniak@gmail.com>
2026-02-11 18:05:30 -0500

    style: clean up rofi styling

--- config/rofi/bin/powermenu.sh
+++ config/rofi/bin/powermenu.sh
@@ -4,13 +4,15 @@ reboot="󰜉    Restart"
 shutdown="⏻    Shutdown"
 
 options="$shutdown\n$reboot"
+options_count=$(wc -l <<< options)
+options_count=$(($options_count + 2))
 
 # Ask for confirmation
 rdialog () {
   rofi -dmenu -i -no-fixed-num-lines -p "Are You Sure? : "
 }
 
-chosen="$(echo -e "$options" | rofi "UP - $uptime" -dmenu -selected-row 0 -i)"
+chosen="$(echo -e "$options" | rofi "UP - $uptime" -dmenu -p "Power Menu" -l $options_count -selected-row -1 -i)"
 case $chosen in
     $shutdown)
       systemctl poweroff
